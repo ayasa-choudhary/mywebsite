72249248e41703fb2f4ef6378cbd97cd36a88446
Ayasa Choudhary <ayasachoudhary@Ayasas-MacBook-Pro.local>
2024-12-18 09:22:11 +0530

    capitalization

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <>
       <div className="container projects my-3" id="projects">
-        <h1>PROJECTS</h1>
+        <h1>Projects</h1>
         <div className="row d-flex justify-content-center align-content-center">
           {project.map((Data) => (
             <>
